1963828a579b5c4af190523ab07a4f3f74b033bc
DerekTBrown <derek@allderek.com>
2015-07-05 20:42:44 -0400

    Add child process to handle updates

--- main.js
+++ main.js
@@ -1,122 +1,98 @@
-/**
+/*
   HamLibJS
-  Simple Interface for accessing the HamLib CLI
-**/
+  A Simple Inteface for HamLib's rigctl interface
+*/
 
-  /* INIT */
+  /* Init */
     var Promise = require('promise');
-    var exec = require('child_process').exec;
-    var spawn = require('child_process').spawn;
+    var Child = require('child_process');
+    var EventEmitter = require('events').EventEmitter;
 
+  /* HamLib Interface */
     var hamlib = {};
 
-    var cmd = "rigctl";
+  /** Rig Object **/
+    hamlib.rig = function(opts){
+      var rig = new EventEmitter();
+          rig.conn = {};
 
-  /** Accessor for Utility Functions **/
-      hamlib.util = {};
+      /* Setup Connection Method */
+          rig.remote = (typeof(opts) !== 'undefined' && opts.hasOwnProperty('remote') && opts.remote);
 
-      /* Get HamLib Version */
-      hamlib.util.version = new Promise(function(fufill, reject){
-        exec(cmd + ' -V', function(error, stdout, stderr){
-          if (error){
-            reject(stderr)
+          // Remote Method
+          if(rig.remote){
+
           }
+
+          // Local Method
           else{
-            fufill(stdout)
-          }
-        });
-      });
-
-
-  /** Create a Radio Connection **/
-  hamlib.rig = function(opts){
-      var rig = {};
-
-      /** Build Args **/
-        args = {
-          "-m" : "1"
-        }
-
-      /** Open Shell **/
-          var ishell = require('child_process').spawn;
-          var child = spawn('rigctl', args);
-
-          res = function(){
-              child.stdout.write("");
+            rig.conn.process = Child.fork('./rig.js', [JSON.stringify(opts)]);
+            rig.conn.sendCommand = function(message){
+              rig.conn.process.send(message);
               return new Promise(function(fufill, reject){
-              child.stdout.on('data', function(data){
-                fufill(data);
+                var listener = function(m){
+                  if (typeof(m) !== 'undefined' && m.hasOwnProperty('cmd') && m.cmd == message.cmd){
+                    rig.conn.process.removeListener('message', listener);
+                    if(m.hasOwnProperty('err')){
+                      reject(m);
+                    }
+                    else{
+                      fufill(m);
+                    }
+                  }
+                };
+                rig.conn.process.on('message', listener);
               });
-              child.stderr.on('data', function(data){
-                reject(code);
-              });
-              child.on('close', function(code){
-                if (code !== 0){
-                  console.log('Process exited with code '+code);
-                  reject(code);
-                }
-              })
-          })};
-
-      /** Abstract Functions **/
-        rig.command = function(command){return new Promise(function(fufill, reject){
-            child.stdin.write(command+"\n");
-            res().done(function(data){
-               data = (data+"").replace(/^.*Rig command:.*$/mg, "").replace(/^\s*[\r\n]/gm, "").replace(/\n$/, "");
-               data = data.split('\n');
-
-               var resobj = {};
-               for (var i = 0; i < data.length; i++){
-                 if (data[i].indexOf(":") > 0){
-                    var ln = data[i].split(':');
-                    resobj[ln[0].trim()] = ln[1].trim();
-                 }
-               }
-
-              fufill(resobj);
-            },
-            function(err){
-              reject(err);
-            });
-        })}
-
-      /** Radio Functions **/
-          // VFO
-          rig.set_frequency = function(freq){ return rig.command('F '+freq)}
-          rig.get_frequency = function(){ return rig.command ('f')}
-          rig.set_mode = function(mode){ return rig.command ('M '+mode)}
-          rig.get_mode = function(){ return rig.command ('m')}
-          rig.set_vfo = function(vfo){ return rig.command ('V '+vfo)}
-          rig.get_vfo = function(){ return rig.command ('v')}
-
-          rig.set_rit = function(rit){ return rig.command ('J '+rit)}
-          rig.get_rit = function(){ return rig.command ('j')}
-          rig.set_xit = function(xit){ return rig.command ('Z '+xit)}
-          rig.get_xit = function(){ return rig.command ('z')}
-
-          rig.set_split_vfo = function(enabled, txvfo){ return rig.command ('S '+enabled+' '+txvfo)}
-          rig.get_split_vfo = function(){ return rig.command ('s')}
-          rig.set_split_freq = function(freq){ return rig.command ('I '+freq)}
-          rig.get_split_freq = function(){ return rig.command ('i')}
-
-          // TX
-          rig.set_ptt = function(ptt){ return rig.command ('T '+ptt)}
-          rig.get_ptt = function(){ return rig.command ('t')}
-          rig.toggle_ptt = function(){
-            return rig.get_ptt.then(function(status){
-                return rig.set_ptt((status.PTT == 0 ? 1 : 0));
-            });
+            }
           }
 
-          // FUNCTIONS
-          rig.set_func = function(func, val){ return rig.command ('U '+func + ' '+val)}
-          rig.get_func = function(func){ return rig.command ('u '+func)}
+      /** Rig Methods **/
+          // VFO
+            rig.get_frequency = function(){return rig.conn.sendCommand({"cmd" : "get_frequency"})}
+            rig.set_frequency = function(frequency){return rig.conn.sendCommand({"cmd" : "set_frequency", "frequency": frequency})}
+            rig.get_mode = function(){return rig.conn.sendCommand({"cmd" : "get_mode"})}
+            rig.set_mode = function(mode){return rig.conn.sendCommand({"cmd" : "set_mode", "mode": mode})}
+            rig.get_vfo = function(){return rig.conn.sendCommand({"cmd" : "get_vfo"})}
+            rig.set_vfo = function(vfo){return rig.conn.sendCommand({"cmd" : "set_vfo", "vfo": vfo})}
 
-          // LEVELS
-          rig.set_level = function(level, val){ return rig.command ('L '+level + ' '+val)}
-          rig.get_level = function(level){ return rig.command ('l '+level)}
+            rig.get_rit = function(){return rig.conn.sendCommand({"cmd" : "get_rit"})}
+            rig.set_rit = function(rit){return rig.conn.sendCommand({"cmd" : "set_rit", "rit": rit})}
+            rig.get_xit = function(){return rig.conn.sendCommand({"cmd" : "get_xit"})}
+            rig.set_xit = function(xit){return rig.conn.sendCommand({"cmd" : "set_xit", "xit": xit})}
+
+            rig.get_split_vfo = function(){return rig.conn.sendCommand({"cmd" : "get_split_vfo"})}
+            rig.set_split_vfo = function(enabled, txvfo){return rig.conn.sendCommand({"cmd" : "set_split_vfo", "enabled": enabled, "txfvo": txvfo})}
+            rig.get_split_freq = function(){return rig.conn.sendCommand({"cmd" : "get_split_freq"})}
+            rig.set_split_freq = function(xit){return rig.conn.sendCommand({"cmd" : "set_split_freq", "freq": freq})}
+
+            rig.get_ptt = function(){return rig.conn.sendCommand({"cmd" : "get_ptt"})}
+            rig.set_ptt = function(xit){return rig.conn.sendCommand({"cmd" : "set_ptt", "ptt": ptt})}
+            rig.toggle_ptt = function(){return rig.conn.sendCommand({"cmd" : "toggle_ptt"})}
+
+            rig.get_func = function(func){return rig.conn.sendCommand({"cmd" : "get_func", "func" : func})}
+            rig.set_func = function(func, val){return rig.conn.sendCommand({"cmd" : "set_func", "func": func, "val" : val})}
+
+            rig.get_level = function(level){return rig.conn.sendCommand({"cmd" : "get_level", "level" : level})}
+            rig.set_level = function(level, val){return rig.conn.sendCommand({"cmd" : "set_level", "level": level, "val" : val})}
 
       return rig;
-  };
+    }
 
-  module.exports = hamlib;
+  /* Rotator Object */
+    hamlib.rot = function(opts){
+      var rot = new EventEmitter();
+      rot.conn = {};
+      rot.remote = (typeof(opts) !== 'undefined' && opts.hasOwnProperty('remote') && opts.remote);
+    }
+
+  /* Server */
+    hamlib.server = function(){
+
+    }
+
+    var rigtest = new hamlib.rig({});
+    rigtest.get_frequency().done(function(m){
+      console.log(m);
+    }, function(err){
+      console.log(err);
+    });
